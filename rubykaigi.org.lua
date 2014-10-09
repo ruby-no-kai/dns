a8a9cb9a96d3090c8ac10e4cf11b66de609d8e99
Kakutani Shintaro <shintaro@kakutani.com>
2014-10-09 14:15:03 +0900

    Initial configuration

---
+++ rubykaigi.org.lua
@@ -0,0 +1,19 @@
+a(_a, "106.187.37.40")
+
+a("j", "106.187.37.40")
+a("linode", "106.187.37.40")
+a("regional", "106.187.37.40")
+a("sapporo", "207.97.227.245")
+a("staging", "106.187.37.40")
+a("tokyo", "106.187.37.40")
+a("yami", "106.187.37.40")
+
+cname("10.tokyo", "regional.rubykaigi.org.")
+cname("2006", _a)
+cname("2007", _a)
+cname("2008", _a)
+cname("blog", "domains.tumblr.com.")
+cname("sendgrid", "sendgrid.net.")
+cname("www", "rubykaigi.org.")
+
+google_app(_a)
