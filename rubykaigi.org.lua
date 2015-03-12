3b5e09b83c9d45310edd7577c086ac75a0f0cee5
Yoji Shidara <dara@shidara.net>
2015-03-12 15:56:33 +0900

    Direct 200[6-8].r.k.o to router

--- rubykaigi.org.lua
+++ rubykaigi.org.lua
@@ -4,9 +4,9 @@ a("linode", "106.187.37.40")
 
 _router = "rko-router.herokuapp.com."
 
-cname("2006", _a)
-cname("2007", _a)
-cname("2008", _a)
+cname("2006", _router)
+cname("2007", _router)
+cname("2008", _router)
 cname("blog", "domains.tumblr.com.")
 cname("sendgrid", "sendgrid.net.")
 cname("www", "rubykaigi.org.")
