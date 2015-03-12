32f09d1af54f44a986e69f1df30ea843fab58d06
Yoji Shidara <dara@shidara.net>
2015-03-12 16:00:06 +0900

    Remove "_" prefix

--- rubykaigi.org.lua
+++ rubykaigi.org.lua
@@ -2,18 +2,18 @@ a(_a, "106.187.37.40")
 
 a("linode", "106.187.37.40")
 
-_router = "rko-router.herokuapp.com."
+router = "rko-router.herokuapp.com."
 
-cname("2006", _router)
-cname("2007", _router)
-cname("2008", _router)
+cname("2006", router)
+cname("2007", router)
+cname("2008", router)
 cname("blog", "domains.tumblr.com.")
 cname("sendgrid", "sendgrid.net.")
 cname("www", "rubykaigi.org.")
-cname("staging", _router)
-cname("sapporo", _router)
-cname("yami", _router)
-cname("j", _router)
-cname("regional", _router)
+cname("staging", router)
+cname("sapporo", router)
+cname("yami", router)
+cname("j", router)
+cname("regional", router)
 
 google_app(_a)
