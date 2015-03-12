888f8a5b185c46b4ab3d2bd29f06eebbb7d054c6
Yoji Shidara <dara@shidara.net>
2015-03-12 15:55:44 +0900

    Extract _router

--- rubykaigi.org.lua
+++ rubykaigi.org.lua
@@ -2,16 +2,18 @@ a(_a, "106.187.37.40")
 
 a("linode", "106.187.37.40")
 
+_router = "rko-router.herokuapp.com."
+
 cname("2006", _a)
 cname("2007", _a)
 cname("2008", _a)
 cname("blog", "domains.tumblr.com.")
 cname("sendgrid", "sendgrid.net.")
 cname("www", "rubykaigi.org.")
-cname("staging", "rko-router.herokuapp.com.")
-cname("sapporo", "rko-router.herokuapp.com.")
-cname("yami", "rko-router.herokuapp.com.")
-cname("j", "rko-router.herokuapp.com.")
-cname("regional", "rko-router.herokuapp.com.")
+cname("staging", _router)
+cname("sapporo", _router)
+cname("yami", _router)
+cname("j", _router)
+cname("regional", _router)
 
 google_app(_a)
