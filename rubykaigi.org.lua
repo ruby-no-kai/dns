3c72569a76100fae416a6ab2bc8f8e0b2be3af6d
hmsk <k.hamasaki@gmail.com>
2015-10-14 17:32:02 +0900

    Add records for validating SendGrid Whitelabels of cfp-app

--- rubykaigi.org.lua
+++ rubykaigi.org.lua
@@ -17,6 +17,11 @@ cname("regional", router)
 cname("regional-gh", "ruby-no-kai.github.io.")
 cname("cfp", router)
 
+-- for SendGrid Whitelabels of cfp-app
+cname("cfp-mail", "u1814470.wl.sendgrid.net")
+cname("s1._domainkey", "s1.domainkey.u1814470.wl.sendgrid.net")
+cname("s2._domainkey", "s2.domainkey.u1814470.wl.sendgrid.net")
+
 -- for Google Apps
 mx(_a, "aspmx.l.google.com", 5)
 mx(_a, "alt1.aspmx.l.google.com", 10)
