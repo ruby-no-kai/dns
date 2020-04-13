418e24f511423fa2703c274f228cd2821adb024f
takahashim <maki@rubycolor.org>
2020-04-13 22:48:51 +0900

    remove comment

--- kaigionrails.org.lua
+++ kaigionrails.org.lua
@@ -1,17 +1,3 @@
-# Original settings (value-domain):
-#
-# a @ 185.199.108.153
-# a @ 185.199.109.153
-# a @ 185.199.110.153
-# a @ 185.199.111.153
-# cname 22161183 case-22161183-for-kaigionrails.org-at.google.com
-# mx ASPMX.L.GOOGLE.COM. 1
-# mx ALT1.ASPMX.L.GOOGLE.COM. 5
-# mx ALT2.ASPMX.L.GOOGLE.COM. 5
-# mx ALT3.ASPMX.L.GOOGLE.COM. 10
-# mx ALT4.ASPMX.L.GOOGLE.COM. 10
-# mx 3asytb3b63jfhxl6yijytjtcgxuftk5orfppfpuiaenyeyxratwa.mx-verification.google.com. 15
-
 a(_a, "185.199.108.153")
 a(_a, "185.199.109.153")
 a(_a, "185.199.110.153")
