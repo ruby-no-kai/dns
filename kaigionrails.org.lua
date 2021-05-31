8172c38ada6425c67cf9e2834db15ee20fc300de
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-05-31 15:26:02 +0900

    Add cfp.kaigionrails.org
    heroku hosting

--- kaigionrails.org.lua
+++ kaigionrails.org.lua
@@ -4,6 +4,7 @@ a(_a, "185.199.110.153")
 a(_a, "185.199.111.153")
 
 cname("past", "ruby-no-kai.github.io.")
+cname("cfp", "fundamental-bedbug-0etq6gplqbbvcenmoum4b7fd.herokudns.com.")
 cname("22161183", "case-22161183-for-kaigionrails.org-at.google.com.")
 
 mx(_a, "aspmx.l.google.com", 1)
