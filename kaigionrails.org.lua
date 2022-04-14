8eab537ed60d707caa5c8b86c7dfc905b741052c
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-04-15 01:31:20 +0900

    Add txt records to kaigionrails.org for mailgun

--- kaigionrails.org.lua
+++ kaigionrails.org.lua
@@ -11,3 +11,6 @@ mx(_a, "alt2.aspmx.l.google.com", 5)
 mx(_a, "alt3.aspmx.l.google.com", 10)
 mx(_a, "alt4.aspmx.l.google.com", 10)
 mx(_a, "3asytb3b63jfhxl6yijytjtcgxuftk5orfppfpuiaenyeyxratwa.mx-verification.google.com", 15)
+
+txt(_a, "v=spf1 include:mailgun.org ~all", 60)
+txt("pic._domainkey", "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCusxsCqLjU5gP+3dwvYAb/gVns8zSJOtDE2EM4RF53TbpXfN5lQJ/PJnmmkONu4QQ38ZaU/JOZarpBG359dn6dT6KMzn07Lhlm88FCAGcuWmGcXemzHZewoOWtjcwXjtLXsMmyb2bEWHdsGWPSqYEg9aRLercMPQKGiQJBFaIz2QIDAQAB", 60)
