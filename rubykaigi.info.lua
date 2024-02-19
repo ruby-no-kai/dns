1fed71fb894ba931d5246d80a6c06c3cba1f414f
takahashim <takahashimm@gmail.com>
2024-02-19 16:22:30 +0900

    Fix TXT record for rubykaigi.info

--- rubykaigi.info.lua
+++ rubykaigi.info.lua
@@ -1,4 +1,4 @@
 alias(_a, "rubykaigi-info.herokuapp.com.")
 cname("www", "rubykaigi-info.herokuapp.com.")
 cname("tokyo10", "tkrk10.herokuapp.com.")
-txt("tokyo10", "google-site-verification=0PWidONZqFxxlj3DDhKOAgmhtsgb-1SDbVmLA-sgEjc")
+txt(_a, "google-site-verification=0PWidONZqFxxlj3DDhKOAgmhtsgb-1SDbVmLA-sgEjc")
